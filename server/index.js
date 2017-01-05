0795f689c5e123d66828410c99c7cb2fa9ad51f5
Hwanghee Kim <claude.kim93@gmail.com>
2017-01-05 14:52:44 +0000

    server scheme

--- server/index.js
+++ server/index.js
@@ -22,17 +22,37 @@ mongoose.connect('localhost', 'tennis');
 // Defining Schemas and Models
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema();
+var userSchema = new Schema({
+    userid : String,
+    name : String,
+    phone : String
+});
 var User = mongoose.model('user', userSchema);
 
-var gameSchema = new Schema();
+var gameSchema = new Schema({
+    type : Boolean,
+    playtime : Date,
+    player1 : String,
+    player2 : String,
+    player3 : String,
+    player4 : String,
+    court : [{type: Schema.ObjectId, ref: 'Court'}],
+    winner : Boolean,
+    isMatched : Boolean,
+    score : String
+});
 var Game = mongoose.model('game', gameSchema);
 
-var courtSchema = new Schema();
+var courtSchema = new Schema({
+    name : String,
+    info : String
+});
 var Court = mongoose.model('court', courtSchema);
+
 
 
 /////////*    Server Implementation     */////////
 
 
 
+app.listen(3000, function() {console.log("Listening on port #3000")});
