f9ebd429a53e6bd51c4dd4c7613fcf086b36a10a
HwangheeKim <hwangheek@gmail.com>
2017-01-06 00:41:41 +0900

    Game Register

---
+++ android/app/src/main/java/com/hwanghee/tennistogether/GameRegister.java
@@ -0,0 +1,13 @@
+package com.hwanghee.tennistogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_register);
+    }
+}
