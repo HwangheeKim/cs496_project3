38fbee9bdc49123c569ea64289b9a89053fef980
imimp1203 <psanha@kaist.ac.kr>
2017-01-11 00:51:53 +0900

    Final GOGOGO

--- android/app/src/main/java/com/hwanghee/tennistogether/MyFirebaseMessagingService.java
+++ android/app/src/main/java/com/hwanghee/tennistogether/MyFirebaseMessagingService.java
@@ -1,6 +1,17 @@
 package com.hwanghee.tennistogether;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -12,10 +23,35 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
 
+//    Vibrator vibrator;
+
+    Notification notification;
+    NotificationManager manager;
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_add)
+                        .setContentTitle(remoteMessage.getNotification().getTitle())
+                        .setContentText(remoteMessage.getNotification().getBody());
+
+
+        Intent notificationIntent = new Intent(this, MyFirebaseMessagingService.class);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        builder.setSound(alarmSound);
+        builder.setContentIntent(contentIntent);
+        builder.setAutoCancel(true);
+        builder.setStyle(new NotificationCompat.InboxStyle());
+// Add as notification
+        manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1, builder.build());
 
         super.onMessageReceived(remoteMessage);
     }
